a20683f2638cbd5b94b03c743d05ba3d7ac18eb9
citrogabriel <citrogabriel@gmail.com>
2024-04-23 18:45:06 -0300

    java

---
+++ main.js
@@ -0,0 +1,24 @@
+alert ("Bienvenido/a")
+let nombre = prompt ("¿Como es tu nombre?")
+
+let edad = prompt ("¿Que edad tenes?")
+
+alert ("Vamos a presentarte las siguientes opciones!")
+
+const color = [ "Azul", "Amarillo", "Verde", "Violeta"]
+color.push("Arena")
+
+console.log (color)
+
+let confirmar = confirm ("Recorda que solo podes elejir 3 colores como maximo!")
+
+let cantidad = "1";
+
+if(cantidad == "1" ){
+    console.log("No aplica descuentos");
+}else{console.warn("Descuento del 30%");
+}
+
+
+
+
